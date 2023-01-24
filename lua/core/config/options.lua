f4c63c51434ce870fd4be8f21909f382000be434
bluereflega <bluereflega0@gmail.com>
2023-01-25 00:42:35 +0800

    feat: add `autochdir` setting

--- lua/core/config/options.lua
+++ lua/core/config/options.lua
@@ -2,14 +2,20 @@ local settings = require("core.config.settings")
 local o = vim.opt
 
 o.cursorline = true -- highlight current line
+o.wrap = false -- disable line wrapping
+
 o.number = settings.number -- show line numbers in gutter
 o.relativenumber = settings.relative_number -- show relative line numbers
-o.wrap = false -- disable line wrapping
+
 o.expandtab = true -- expand tabs to spaces
 o.tabstop = 4 -- expand tabs to 4 spaces
 o.smarttab = true -- use smart tabbing
+
 o.shiftwidth = 4 -- insert 4 spaces for indents
 o.autoindent = true -- automatically indent new lines
 o.smartindent = true -- use smart indenting
+
 o.termguicolors = true -- idk what this is exactly
 o.clipboard = "unnamedplus" -- let nvim access os clipboard
+
+o.autochdir = settings.autochdir -- automatically change working directory
--- lua/core/config/settings.lua
+++ lua/core/config/settings.lua
@@ -4,6 +4,7 @@ M.theme = "onedark" -- theme
 M.transparent = false -- transparency
 M.number = true -- show line numbers in gutter
 M.relative_number = true -- show relative line numbers
+M.autochdir = true -- automatically change working directory
 
 function M.is_theme(name)
     return name == M.theme
