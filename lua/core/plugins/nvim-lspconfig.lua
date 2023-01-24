6db4be448498bd45c4ef458730c5942a73ab6946
bluereflega <bluereflega0@gmail.com>
2023-01-25 00:44:53 +0800

    feat: add nvim-lspconfig plugin

---
+++ lua/core/plugins/nvim-lspconfig.lua
@@ -0,0 +1,16 @@
+local M = {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        -- TODO: add mason.nvim and cmp-nvim-lsp
+        "onsails/lspkind-nvim",
+        { "folke/neodev.nvim", config = true },
+    },
+    opts = {
+        diagnostics = {},
+        format = {},
+        servers = require("core.plugins.lsp.servers"),
+        setup = {},
+    },
+}
+
+return M
