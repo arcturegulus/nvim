368d08630c7489a1ccd2e609333133c0e2559a58
bluereflega <bluereflega0@gmail.com>
2023-01-25 00:58:01 +0800

    feat: add Lua, C++, and Python langserver configs

--- lua/core/plugins/lsp/servers/init.lua
+++ lua/core/plugins/lsp/servers/init.lua
@@ -1,5 +1,15 @@
 local M = {
+    -- clangd (c/c++)
+    clangd = require("core.plugins.lsp.servers.clangd"),
 
+    -- omnisharp (c#)
+    -- omnisharp = require("core.plugins.lsp.servers.omnisharp"),
+
+    -- pyright (python)
+    pyright = require("core.plugins.lsp.servers.pyright"),
+
+    -- lua-language-server (lua)
+    sumneko_lua = require("core.plugins.lsp.servers.lua-language-server"),
 }
 
 return M
---
+++ lua/core/plugins/lsp/servers/lua-language-server.lua
@@ -0,0 +1,21 @@
+local M = {
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                globals = { "vim" },
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+}
+
+return M
