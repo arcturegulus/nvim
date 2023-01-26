e24e79fbc0e9551db1b745b7c328f4323366988e
bluereflega <bluereflega0@gmail.com>
2023-01-26 08:08:59 +0800

    feat: add mason.nvim and mason-lspconfig

--- lua/core/plugins/nvim-lspconfig.lua
+++ lua/core/plugins/nvim-lspconfig.lua
@@ -1,9 +1,12 @@
+-- TODO: add cmp-nvim-lsp
+
 local M = {
     "neovim/nvim-lspconfig",
     dependencies = {
-        -- TODO: add mason.nvim and cmp-nvim-lsp
         "onsails/lspkind-nvim",
         { "folke/neodev.nvim", config = true },
+        "mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
     },
     opts = {
         diagnostics = {},
@@ -11,6 +14,49 @@ local M = {
         servers = require("core.plugins.lsp.servers"),
         setup = {},
     },
+    config = function(_, opts)
+        local mlsp = require("mason-lspconfig")
+        local ensure_installed = {}
+        local servers = opts.servers
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        local available = mlsp.get_available_servers()
+        local function setup(server)
+            local server_opts = servers[server] or {}
+            server_opts.capabilities = capabilities
+
+            if opts.setup[server] then
+                -- use server's custom setup handler in opts.setup if it exists
+                if opts.setup[server](server, server_opts) then
+                    return
+                end
+            elseif opts.setup["*"] then
+                -- use custom general setup handler if specified
+                if opts.setup["*"](server, server_opts) then
+                    return
+                end
+            end
+
+            -- if neither, setup server through nvim-lspconfig
+            require("lspconfig")[server].setup(server_opts)
+        end
+
+        for server, server_opts in pairs(servers) do
+            if server_opts then
+                server_opts = server_opts == true and {} or server_opts
+
+                -- manually setup server if mason == false or if
+                -- server cannot be installed with mason-lspconfig
+                if server_opts.mason == false or not vim.tbl_contains(available, server) then
+                    setup(server)
+                else
+                    ensure_installed[#ensure_installed + 1] = server
+                end
+            end
+        end
+
+        mlsp.setup({ ensure_installed = ensure_installed })
+        mlsp.setup_handlers({ setup })
+    end,
     event = "BufReadPre",
 }
 
