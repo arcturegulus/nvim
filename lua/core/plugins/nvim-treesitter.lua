cef7d18895a5096df075461bb7e5e87e96d9050b
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-01-29 21:30:22 +0800

    feat: add nvim-treesitter

---
+++ lua/core/plugins/nvim-treesitter.lua
@@ -0,0 +1,31 @@
+local M = {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+        ensure_installed = {
+            "c",
+            "lua",
+            "vim",
+            "help",
+
+            "cpp",
+            "c_sharp",
+            "comment",
+            "python",
+        },
+        auto_install = true,
+        highlight = {
+            enable = true,
+        },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+
+        require("nvim-treesitter.install").prefer_git = false
+        require("nvim-treesitter.install").compilers = { "clang", "gcc" }
+
+        vim.cmd [[TSUpdate]]
+    end,
+    event = "VeryLazy",
+}
+
+return M
