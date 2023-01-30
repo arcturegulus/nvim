71df350b535ed80b016272aaa68c51490fc96788
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-01-31 01:52:32 +0800

    refactor: disable `autochdir`

--- lua/core/config/settings.lua
+++ lua/core/config/settings.lua
@@ -4,7 +4,7 @@ M.theme = "onedark" -- theme
 M.transparent = false -- transparency
 M.number = true -- show line numbers in gutter
 M.relative_number = true -- show relative line numbers
-M.autochdir = true -- automatically change working directory
+M.autochdir = false -- automatically change working directory
 
 function M.is_theme(name)
     return name == M.theme
