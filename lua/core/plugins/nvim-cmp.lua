d476aa797967cc2650d0f01657e23ac6b906a167
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-01-30 13:40:17 +0800

    feat: add nvim-cmp sample mappings with LuaSnip

--- lua/core/plugins/nvim-cmp.lua
+++ lua/core/plugins/nvim-cmp.lua
@@ -54,6 +54,12 @@ M.dependencies = {
 }
 
 M.opts = function()
+    local has_words_before = function()
+        unpack = unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+
     local cmp = require("cmp")
     local luasnip = require("luasnip")
 
@@ -74,18 +80,17 @@ M.opts = function()
             ["<C-e>"] = cmp.mapping.abort(),
             ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-            -- custom mappings from lunarvim
             ["<Tab>"] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_next_item()
-                elseif luasnip.expandable() then
-                    luasnip.expand()
                 elseif luasnip.expand_or_jumpable() then
                     luasnip.expand_or_jump()
+                elseif has_words_before() then
+                    cmp.complete()
                 else
                     fallback()
                 end
-            end),
+            end, { "i", "s" }),
             ["<S-Tab>"] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_prev_item()
@@ -94,7 +99,7 @@ M.opts = function()
                 else
                     fallback()
                 end
-            end),
+            end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
             { name = "nvim_lsp" },
