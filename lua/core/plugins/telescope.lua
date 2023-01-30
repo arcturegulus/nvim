5410547835bdea7a22ca4ff30b4d6879046967ae
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-01-30 22:02:51 +0800

    feat: add telescope.nvim

---
+++ lua/core/plugins/telescope.lua
@@ -0,0 +1,11 @@
+local M = {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+        "plenary.nvim",
+        "nvim-telescope/telescope-fzf-native.nvim",
+    },
+    version = false,
+    cmd = "Telescope",
+}
+
+return M
