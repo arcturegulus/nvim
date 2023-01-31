2e32309219ce94dc53f7e2d796e825e41066ffc1
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-01-31 23:28:59 +0800

    feat: add which-key.nvim

---
+++ lua/core/plugins/which-key.lua
@@ -0,0 +1,16 @@
+local M = {
+    "folke/which-key.nvim",
+    opts = {
+        disable = {
+            filetypes = { "TelescopePrompt" },
+        },
+    },
+    config = function(_, opts)
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+        require("which-key").setup(opts)
+    end,
+    event = "VeryLazy",
+}
+
+return M
