b0333e05f580fdbbebd2d17f8539b47c8f3e1e39
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-02-05 13:32:59 +0800

    feat: add everblush colorscheme

--- lua/core/plugins/colorscheme.lua
+++ lua/core/plugins/colorscheme.lua
@@ -52,4 +52,20 @@ return {
         end,
         event = "VeryLazy",
     },
+
+    -- everblush
+    {
+        "Everblush/nvim",
+        name = "everblush",
+        lazy = not settings.is_theme("everblush"),
+        opts = {
+            transparent_background = settings.transparent,
+        },
+        config = function(_, opts)
+            require("everblush").setup(opts)
+            if settings.is_theme("everblush") then
+                vim.cmd [[colorscheme everblush]]
+            end
+        end
+    },
 }
