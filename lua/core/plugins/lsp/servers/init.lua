19202857f98c2170fa24a51aa328736f99488517
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-02-15 22:37:28 +0800

    fix(lsp): rename sumneko_lua > lua_ls

--- lua/core/plugins/lsp/servers/init.lua
+++ lua/core/plugins/lsp/servers/init.lua
@@ -3,6 +3,6 @@ local M = {}
 M.clangd = require("core.plugins.lsp.servers.clangd") -- clangd (c/c++)
 -- M.omnisharp = require("core.plugins.lsp.servers.omnisharp") -- omnisharp (c#)
 M.pyright = require("core.plugins.lsp.servers.pyright") -- pyright (python)
-M.sumneko_lua = require("core.plugins.lsp.servers.lua-language-server") -- lua-language-server (lua)
+M.lua_ls = require("core.plugins.lsp.servers.lua-language-server") -- lua-language-server (lua)
 
 return M
