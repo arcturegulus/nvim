e099f38e3eb8abc8091cb8d34e7caa0aff9f88fb
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-03-05 20:44:28 +0800

    refactor(settings): put comments in new lines

--- lua/core/config/settings.lua
+++ lua/core/config/settings.lua
@@ -1,10 +1,19 @@
 local M = {}
 
-M.theme = "rose-pine" -- theme on startup
-M.transparent = true -- transparency
-M.number = true -- show line numbers in gutter
-M.relative_number = true -- show relative line numbers
-M.autochdir = false -- automatically change working directory
+-- theme on startup
+M.theme = "rose-pine"
+
+-- transparency
+M.transparent = true
+
+-- show line numbers in gutter
+M.number = true
+
+-- show relative line numbers
+M.relative_number = true
+
+-- automatically change working directory
+M.autochdir = false
 
 function M.is_theme(name)
     return name == M.theme
