04d024a37450bc27f4f7c7ddf0e7847a4f061478
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-03-10 00:38:16 +0800

    style(options): place comments in same line as options

--- lua/core/config/settings.lua
+++ lua/core/config/settings.lua
@@ -1,19 +1,10 @@
 local M = {}
 
--- theme on startup
-M.theme = "rose-pine"
-
--- transparency
-M.transparent = true
-
--- show line numbers in gutter
-M.number = true
-
--- show relative line numbers
-M.relative_number = true
-
--- automatically change working directory
-M.autochdir = false
+M.theme = "rose-pine" -- theme on startup
+M.transparent = false -- transparency
+M.number = true -- show line numbers in gutter
+M.relative_number = true -- show relative line numbers
+M.autochdir = false -- automatically change working directory
 
 function M.is_theme(name)
     return name == M.theme
