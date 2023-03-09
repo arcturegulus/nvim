e44ca482da1bdaa14667c6fa10878806594f963f
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-03-10 01:04:51 +0800

    feat: add neovide config

--- lua/core/config/settings.lua
+++ lua/core/config/settings.lua
@@ -5,6 +5,7 @@ M.transparent = false -- transparency
 M.number = true -- show line numbers in gutter
 M.relative_number = true -- show relative line numbers
 M.autochdir = false -- automatically change working directory
+M.guifont = "JetbrainsMono Nerd Font, JetBrains Mono:#h-none" -- font for nvim gui (primarily neovide)
 
 function M.is_theme(name)
     return name == M.theme
