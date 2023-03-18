ddab455610b61ee3b74fdb930da295a89632c905
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-03-18 19:22:42 +0800

    feat: add global switch function

--- lua/core/config/functions.lua
+++ lua/core/config/functions.lua
@@ -1,5 +1,12 @@
 local M = {}
 
+function M.switch(param, case_table)
+    local case = case_table[param]
+    if case then return case() end
+    local def = case_table["default"]
+    return def and def() or nil
+end
+
 function M.get_neo_tree_width()
     local width = math.floor(vim.go.columns * 0.25)
     local width_str
