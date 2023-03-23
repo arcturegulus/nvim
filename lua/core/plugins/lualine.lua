741d49b868bae76747c54c5b5bca6b12ab50e88d
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-03-23 14:47:45 +0800

    Add custom diff source using gitsigns

--- lua/core/plugins/lualine.lua
+++ lua/core/plugins/lualine.lua
@@ -1,6 +1,20 @@
+local function diff_source()
+    local gitsigns = vim.b.gitsigns_status_dict
+    if gitsigns then
+        return {
+            added = gitsigns.added,
+            modified = gitsigns.modified,
+            removed = gitsigns.removed,
+        }
+    end
+end
+
 local M = {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-web-devicons" },
+    dependencies = {
+        "gitsigns.nvim",
+        "nvim-web-devicons",
+    },
     opts = {
         options = {
             theme = "auto",
