474d7ed720c9432f97efc93ed7f25144531f6659
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-03-24 02:46:49 +0800

    Pass function to nvim-tree width instead of int

--- lua/core/plugins/nvim-tree.lua
+++ lua/core/plugins/nvim-tree.lua
@@ -9,7 +9,7 @@ local M = {
         sync_root_with_cwd = true,
         view = {
             hide_root_folder = true,
-            width = f.get_nvim_tree_width();
+            width = f.get_nvim_tree_width;
         },
         renderer = {
             group_empty = true,
