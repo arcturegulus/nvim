954c1ccff5203ab624f4b73c84f4e33b5ef79ca7
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-03-24 02:39:13 +0800

    Add comments for each autocmd

--- lua/core/config/autocmds.lua
+++ lua/core/config/autocmds.lua
@@ -4,6 +4,7 @@ local function augroup(name)
     return vim.api.nvim_create_augroup(name, { clear = true })
 end
 
+-- auto-resize nvim-tree when resizing window
 vim.api.nvim_create_autocmd("VimResized", {
     group = augroup("AutoNvimTreeResize"),
     callback = function ()
