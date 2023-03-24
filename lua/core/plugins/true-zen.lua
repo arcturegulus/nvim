5510caf70968a37ea6fa0ff2cf7b98c70f51fd87
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-03-25 06:52:09 +0800

    Fix true-zen minimalist mode not hiding tree

--- lua/core/plugins/true-zen.lua
+++ lua/core/plugins/true-zen.lua
@@ -1,3 +1,6 @@
+local ntview = require("nvim-tree.view")
+local ntapi = require("nvim-tree.api")
+
 local M = {
     "Pocco81/true-zen.nvim",
     opts = {
@@ -5,7 +8,16 @@ local M = {
             minimalist = {
                 callbacks = {
                     open_pre = function()
-                        vim.cmd [[NeoTreeClose]]
+                        if ntview.is_visible() then
+                            vim.g.tree_open_before_tz = true
+                            ntapi.tree.close()
+                        end
+                    end,
+                    close_pos = function()
+                        if vim.g.tree_open_before_tz and not ntview.is_visible() then
+                            vim.g.tree_open_before_tz = false
+                            ntapi.tree.open()
+                        end
                     end,
                 },
             },
