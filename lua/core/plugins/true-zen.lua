3b7bae849ccb3de926040d224b6a48909c9a4f47
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-03-29 21:35:11 +0800

    Hide nvim-tree on entering true-zen minimalist & ataraxis modes

--- lua/core/plugins/true-zen.lua
+++ lua/core/plugins/true-zen.lua
@@ -1,24 +1,35 @@
 local ntview = require("nvim-tree.view")
 local ntapi = require("nvim-tree.api")
 
+local function close_nvim_tree()
+    if ntview.is_visible() then
+        vim.g.tree_open_before_tz = true
+        ntapi.tree.close()
+    end
+end
+
+local function open_nvim_tree()
+    if vim.g.tree_open_before_tz and not ntview.is_visible() then
+        vim.g.tree_open_before_tz = false
+        ntapi.tree.open()
+    end
+end
+
 local M = {
     "Pocco81/true-zen.nvim",
+    dependencies = { "twilight.nvim" },
     opts = {
         modes = {
+            ataraxis = {
+                callbacks = {
+                    open_pre = close_nvim_tree(),
+                    close_pos = open_nvim_tree(),
+                },
+            },
             minimalist = {
                 callbacks = {
-                    open_pre = function()
-                        if ntview.is_visible() then
-                            vim.g.tree_open_before_tz = true
-                            ntapi.tree.close()
-                        end
-                    end,
-                    close_pos = function()
-                        if vim.g.tree_open_before_tz and not ntview.is_visible() then
-                            vim.g.tree_open_before_tz = false
-                            ntapi.tree.open()
-                        end
-                    end,
+                    open_pre = close_nvim_tree(),
+                    close_pos = open_nvim_tree(),
                 },
             },
         },
