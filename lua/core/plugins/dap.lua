4ac3ed5215bb1048b5431dee47871e6e806efa55
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-04-17 15:14:47 +0800

    Add nvim-dap

---
+++ lua/core/plugins/dap.lua
@@ -0,0 +1,37 @@
+local M = {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "theHamsta/nvim-dap-virtual-text",
+    },
+    config = function()
+        local dap = require("dap")
+
+        dap.adapters.codelldb = {
+            type = "server",
+            port = "${port}",
+            executable = {
+                command = "/usr/bin/codelldb",
+                args = { "--port", "${port}" },
+            },
+        }
+
+        dap.configurations.cpp = {
+            {
+                name = "Launch",
+                type = "codelldb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                end,
+                cwd = "${workspaceFolder}",
+                stopOnEntry = false,
+            },
+        }
+
+        dap.configurations.c = dap.configurations.cpp
+    end,
+    event = "VeryLazy",
+}
+
+return M
