2d01d7c38b1b5ffea9e983350b2e25165b2255c4
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-05-07 07:51:55 +0800

    Sort lsp servers alphabetically

--- lua/core/plugins/lsp/servers/init.lua
+++ lua/core/plugins/lsp/servers/init.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 M.clangd = require("core.plugins.lsp.servers.clangd") -- clangd (c/c++)
+M.lua_ls = require("core.plugins.lsp.servers.lua-language-server") -- lua-language-server (lua)
 -- M.omnisharp = require("core.plugins.lsp.servers.omnisharp") -- omnisharp (c#)
 M.pyright = require("core.plugins.lsp.servers.pyright") -- pyright (python)
-M.lua_ls = require("core.plugins.lsp.servers.lua-language-server") -- lua-language-server (lua)
 
 return M
