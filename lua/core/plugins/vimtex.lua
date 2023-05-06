90276d7fd932a5524dc7b38ae81f63a12049de7e
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-05-06 23:08:02 +0800

    Disable lazy-loading for vimtex

--- lua/core/plugins/vimtex.lua
+++ lua/core/plugins/vimtex.lua
@@ -1,5 +1,6 @@
 local M = {
     "lervag/vimtex",
+    lazy = false,
     config = function()
         vim.g.vimtex_view_general_viewer = "qpdfview"
         vim.g.vimtex_view_general_options = "--unique @pdf\\#src:@tex:@line:@col"
@@ -7,7 +8,6 @@ local M = {
         vim.g.vimtex_compiler_method = "latexmk"
     end,
     ft = "tex",
-    event = "BufReadPre",
 }
 
 return M
