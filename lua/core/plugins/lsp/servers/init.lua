a758a2e0158be0906245faa749f9606226d4f793
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-05-16 05:26:41 +0800

    Add rust-analyzer lsp

--- lua/core/plugins/lsp/servers/init.lua
+++ lua/core/plugins/lsp/servers/init.lua
@@ -4,5 +4,6 @@ M.clangd = require("core.plugins.lsp.servers.clangd") -- clangd (c/c++)
 M.lua_ls = require("core.plugins.lsp.servers.lua-language-server") -- lua-language-server (lua)
 -- M.omnisharp = require("core.plugins.lsp.servers.omnisharp") -- omnisharp (c#)
 M.pyright = require("core.plugins.lsp.servers.pyright") -- pyright (python)
+M.rust_analyzer = require("core.plugins.lsp.servers.rust-analyzer") -- rust-analyzer (rust)
 
 return M
