afe227f825d2b8bf9cf1e2441c1bac52cb235084
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-05-17 08:34:48 +0800

    Add toggleterm configuration

--- lua/core/plugins/toggleterm.lua
+++ lua/core/plugins/toggleterm.lua
@@ -1,5 +1,16 @@
 local M = {
     "akinsho/toggleterm.nvim",
+    opts = {
+        size = function(term)
+            if term.direction == "vertical" then
+                return vim.o.columns * 0.4
+            else
+                return 20
+            end
+        end,
+        hide_numbers = true,
+        shade_terminals = true,
+    },
     event = "VeryLazy",
 }
 
