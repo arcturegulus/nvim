182d2b012fcf101b698a82347ddaef7183065c45
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-05-17 10:59:48 +0800

    Style nvim-cmp `init.lua`
    small stuff

--- lua/core/plugins/cmp/init.lua
+++ lua/core/plugins/cmp/init.lua
@@ -14,14 +14,14 @@ M.dependencies = {
 }
 
 M.opts = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
     local has_words_before = function()
         unpack = unpack
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
-
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
 
     return {
         snippet = {
@@ -51,6 +51,7 @@ M.opts = function()
                     fallback()
                 end
             end, { "i", "s" }),
+
             ["<S-Tab>"] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_prev_item()
@@ -65,8 +66,8 @@ M.opts = function()
             { name = "nvim_lsp" },
             { name = "omni" },
             { name = "luasnip" },
-            { name = "path" },
             { name = "nvim_lua" },
+            { name = "path" },
         }, {
             { name = "buffer" },
         }),
