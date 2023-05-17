dcc63d81ce8cf8e5eccad077c87246161f654c23
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-05-17 11:00:53 +0800

    Use capabilities from cmp-nvim-lsp

--- lua/core/plugins/lsp/servers/jsonls.lua
+++ lua/core/plugins/lsp/servers/jsonls.lua
@@ -1,4 +1,4 @@
-local capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local M = {
