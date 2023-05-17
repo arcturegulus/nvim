4c7ebc652c947f310ecd5c9258cd8a7f396573ce
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-05-17 08:35:01 +0800

    Add jsonls

--- lua/core/plugins/lsp/servers/init.lua
+++ lua/core/plugins/lsp/servers/init.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 M.clangd = require("core.plugins.lsp.servers.clangd") -- clangd (c/c++)
+M.jsonls = require("core.plugins.lsp.servers.jsonls") -- json-lsp (json)
 M.lua_ls = require("core.plugins.lsp.servers.lua-language-server") -- lua-language-server (lua)
 -- M.omnisharp = require("core.plugins.lsp.servers.omnisharp") -- omnisharp (c#)
 M.pyright = require("core.plugins.lsp.servers.pyright") -- pyright (python)
---
+++ lua/core/plugins/lsp/servers/jsonls.lua
@@ -0,0 +1,8 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+local M = {
+    capabilities = capabilities,
+}
+
+return M
