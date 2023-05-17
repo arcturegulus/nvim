5dc0fa3507e6a2d00499cfa57ded36c34d33f7d6
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-05-18 06:46:05 +0800

    Add nightfox.nvim colorscheme

--- lua/core/plugins/colorscheme.lua
+++ lua/core/plugins/colorscheme.lua
@@ -49,6 +49,20 @@ return {
         event = "VeryLazy",
     },
 
+    -- nightfox
+    {
+        "EdenEast/nightfox.nvim",
+        lazy = not settings.is_theme("nightfox"),
+        opts = {},
+        config = function(_, opts)
+            require("nightfox").setup(opts)
+            if settings.is_theme("nightfox") then
+                vim.cmd [[colorscheme nightfox]]
+            end
+        end,
+        event = "VeryLazy",
+    },
+
     -- rose-pine
     {
         "rose-pine/neovim",
