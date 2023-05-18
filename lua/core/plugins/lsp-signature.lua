a381e74586e107a44d601821b689daf435a192ef
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-05-18 09:51:04 +0800

    Add lsp_signature.nvim

---
+++ lua/core/plugins/lsp-signature.lua
@@ -0,0 +1,12 @@
+local M = {
+    "ray-x/lsp_signature.nvim",
+    opts = {
+        bind = true,
+        handler_opts = {
+            border = "rounded"
+        }
+    },
+    event = "VeryLazy",
+}
+
+return M
