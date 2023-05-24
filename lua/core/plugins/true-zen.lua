2ce2595ef8e62b6531bc6e0300f21da0b7908e89
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-05-24 13:14:24 +0800

    Fix true-zen breaking without nvim-tree
    Now neo-tree is used.

--- lua/core/plugins/true-zen.lua
+++ lua/core/plugins/true-zen.lua
@@ -1,37 +1,13 @@
-local ntview = require("nvim-tree.view")
-local ntapi = require("nvim-tree.api")
-
-local function close_nvim_tree()
-  if ntview.is_visible() then
-    vim.g.tree_open_before_tz = true
-    ntapi.tree.close()
-  end
-end
-
-local function open_nvim_tree()
-  if vim.g.tree_open_before_tz and not ntview.is_visible() then
-    vim.g.tree_open_before_tz = false
-    ntapi.tree.open()
-  end
-end
-
 local M = {
   "Pocco81/true-zen.nvim",
-  dependencies = { "twilight.nvim" },
+  dependencies = {
+    "twilight.nvim",
+    "neo-tree.nvim",
+  },
   opts = {
     modes = {
       ataraxis = {
         quit_untoggles = false,
-        callbacks = {
-          open_pre = close_nvim_tree(),
-          close_pos = open_nvim_tree(),
-        },
-      },
-      minimalist = {
-        callbacks = {
-          open_pre = close_nvim_tree(),
-          close_pos = open_nvim_tree(),
-        },
       },
     },
     integrations = {
@@ -40,6 +16,51 @@ local M = {
       lualine = true,
     },
   },
+  config = function(_, opts)
+    local manager = require("neo-tree.sources.manager")
+    local renderer = require("neo-tree.ui.renderer")
+
+    -- close neo-tree if it is open
+    local function close_filetree()
+      local state = manager.get_state("filesystem")
+      local window_exists = renderer.window_exists(state)
+
+      if window_exists then
+        vim.g.tree_open_before_tz = true
+        vim.cmd("Neotree close")
+      end
+    end
+
+    -- open neo-tree if it was open before
+    local function open_filetree()
+      local state = manager.get_state("filesystem")
+      local window_exists = renderer.window_exists(state)
+
+      if vim.g.tree_open_before_tz and not window_exists then
+        vim.g.tree_open_before_tz = false
+        vim.cmd("Neotree show")
+      end
+    end
+
+    local config = vim.tbl_deep_extend("force", opts, {
+      modes = {
+        ataraxis = {
+          callbacks = {
+            open_pre = close_filetree,
+            close_pos = open_filetree,
+          },
+        },
+        minimalist = {
+          callbacks = {
+            open_pre = close_filetree,
+            close_pos = open_filetree,
+          },
+        },
+      },
+    })
+
+    require("true-zen").setup(config)
+  end,
   event = "VeryLazy",
 }
 
