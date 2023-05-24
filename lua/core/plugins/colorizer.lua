7a9b123e8e145c7af797716ffa6beef9577448cd
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-05-24 09:25:59 +0800

    Configure colorizer
    - Don't colorize names
    
    - Colorize foreground and not background

--- lua/core/plugins/colorizer.lua
+++ lua/core/plugins/colorizer.lua
@@ -1,5 +1,11 @@
 local M = {
   "NvChad/nvim-colorizer.lua",
+  opts = {
+    user_default_options = {
+      names = false,
+      mode = "foreground",
+    },
+  },
   config = true,
   event = "VeryLazy",
 }
