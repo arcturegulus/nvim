3036ca8cb4a653b0886d54fb6455ef1e4808aea5
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-05-30 05:48:48 +0800

    Add coq_nvim
    Still needs a lot of tweaking in the settings but it works for now.

---
+++ lua/core/plugins/mason-lspconfig.lua
@@ -0,0 +1,33 @@
+local M = {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "mason.nvim",
+    "neovim/nvim-lspconfig",
+    "coq_nvim",
+  },
+  config = function()
+    local mlsp = require("mason-lspconfig")
+    local coq = require("coq")
+    local servers = require("core.plugins.lsp.servers")
+    local ensure_installed = {}
+
+    for server, _ in pairs(servers) do
+      ensure_installed[#ensure_installed + 1] = server
+    end
+
+    local handlers = {
+      -- default setup handler
+      function(server)
+        local server_opts = servers[server] or {}
+        require("lspconfig")[server].setup(coq.lsp_ensure_capabilities(server_opts))
+      end,
+      -- dedicated handlers for specific servers here
+    }
+
+    mlsp.setup({ ensure_installed = ensure_installed })
+    mlsp.setup_handlers(handlers)
+  end,
+  event = "VeryLazy",
+}
+
+return M
