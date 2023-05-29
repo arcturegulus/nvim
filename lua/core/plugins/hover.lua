e54532dc8be188375abdffdfcb99c832a5eb292b
bluereflega <43960495+bluereflega@users.noreply.github.com>
2023-05-30 05:49:28 +0800

    Add hover.nvim
    Use with "<Leader>k" and/or "<Leader>K".

---
+++ lua/core/plugins/hover.lua
@@ -0,0 +1,24 @@
+local M = {
+  "lewis6991/hover.nvim",
+  opts = {
+    init = function()
+      require("hover.providers.lsp")
+    end,
+    preview_opts = {
+      border = "rounded",
+    },
+    title = true,
+  },
+  config = function(_, opts)
+    local hover = require("hover")
+    local utils = require("core.config.utils")
+
+    hover.setup(opts)
+
+    utils.map("n", "<Leader>k", hover.hover, "preview symbol")
+    utils.map("n", "<Leader>K", hover.hover_select, "preview symbol (select source)")
+  end,
+  event = "VeryLazy",
+}
+
+return M
