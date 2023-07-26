be72b6ab4fa2e2075eafebc16c441a7a705a8aa1
Paul Lisondra <arct@arcturegulus.space>
2023-07-27 05:21:40 +0800

    Improve appearance when using neovide

--- lua/core/config/options.lua
+++ lua/core/config/options.lua
@@ -28,8 +28,8 @@ vim.g.editorconfig = true -- enable .editorconfig
 -- neovide configuration
 if vim.g.neovide then
   vim.o.guifont = settings.guifont
-  vim.g.neovide_scale_factor = 0.6
-  vim.g.neovide_refresh_rate = 60
+  vim.g.neovide_scale_factor = 1
+  vim.g.neovide_refresh_rate = 165
   vim.g.neovide_cursor_animation_length = 0.13
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_cursor_trail_size = 0
--- lua/core/config/settings.lua
+++ lua/core/config/settings.lua
@@ -5,7 +5,7 @@ M.transparent = false -- transparency
 M.number = true -- show line numbers in gutter
 M.relative_number = true -- show relative line numbers
 M.autochdir = false -- automatically change working directory
-M.guifont = "JetbrainsMono Nerd Font, JetBrains Mono:#h-none" -- font for nvim gui (primarily neovide)
+M.guifont = "bluesevka:h10.5:#h-none, JetbrainsMono Nerd Font:h10.5:#h-none" -- font for nvim gui (primarily neovide)
 M.disable_notify_nvim = false -- disable notify.nvim plugin
 
 function M.is_theme(name)
