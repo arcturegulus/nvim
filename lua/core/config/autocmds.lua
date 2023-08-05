f43e4d561c9888ec81ead5b5e507bed9828d7b93
Paul Lisondra <paullisondra@proton.me>
2023-08-05 19:34:16 +0800

    Remove unused barbar.nvim autocommand

--- lua/core/config/autocmds.lua
+++ lua/core/config/autocmds.lua
@@ -12,38 +12,6 @@ vim.api.nvim_create_autocmd("VimResized", {
   end,
 })
 
--- barbar.nvim offset
--- vim.api.nvim_create_autocmd("FileType", {
---     group = augroup("BarbarOffset"),
---     callback = function(tbl)
---         local set_offset = require("bufferline.api").set_offset
---
---         local bufwinid
---         local last_width
---         local autocmd = vim.api.nvim_create_autocmd("WinScrolled", {
---             callback = function()
---                 bufwinid = bufwinid or vim.fn.bufwinid(tbl.buf)
---
---                 local width = vim.api.nvim_win_get_width(bufwinid)
---                 if width ~= last_width then
---                     set_offset(width + 1)
---                     last_width = width
---                 end
---             end,
---         })
---
---         vim.api.nvim_create_autocmd("BufWipeout", {
---             buffer = tbl.buf,
---             callback = function()
---                 vim.api.nvim_del_autocmd(autocmd)
---                 set_offset(0)
---             end,
---             once = true,
---         })
---     end,
---     pattern = "NvimTree",
--- })
-
 -- auto-initialize vim-pencil for text filetypes
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup("AutoPencil"),
