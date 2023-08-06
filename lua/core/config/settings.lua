50f62c7048e636280acfc59822375faf4ecb81f0
Paul Lisondra <arct@arcturegulus.space>
2023-08-06 21:55:18 +0800

    Remove some themes
    Removes neovim-ayu, nightfox.nvim, nightly.nvim, Rosé Pine,
    and tokyonight.nvim. Apart from nightly.nvim, I do not use those
    themes much. As for nightly.nvim, Everblush is already present
    in my config and nightly.nvim is just a fork of that.

--- lua/core/config/settings.lua
+++ lua/core/config/settings.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-M.theme = "nightly" -- theme on startup
+M.theme = "catppuccin" -- theme on startup
 M.transparent = false -- transparency
 M.number = true -- show line numbers in gutter
 M.relative_number = true -- show relative line numbers
--- lua/core/plugins/colorschemes/init.lua
+++ lua/core/plugins/colorschemes/init.lua
@@ -1,13 +1,8 @@
 local M = {
-  require("core.plugins.colorschemes.ayu"),
   require("core.plugins.colorschemes.catppuccin"),
   require("core.plugins.colorschemes.everblush"),
   require("core.plugins.colorschemes.material"),
-  require("core.plugins.colorschemes.nightfox"),
-  require("core.plugins.colorschemes.nightly"),
   require("core.plugins.colorschemes.onedark"),
-  require("core.plugins.colorschemes.rose-pine"),
-  require("core.plugins.colorschemes.tokyonight"),
 }
 
 return M
