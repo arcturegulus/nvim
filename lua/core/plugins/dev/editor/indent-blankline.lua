e32abb9d3e76197c21170d9b849c692a62ed6034
Paul Lisondra <paullisondra@proton.me>
2023-11-19 11:28:07 +0800

    Update indent-blankline.nvim to version 3 config

--- lua/core/plugins/dev/editor/indent-blankline.lua
+++ lua/core/plugins/dev/editor/indent-blankline.lua
@@ -2,22 +2,29 @@
 local M = {
   "lukas-reineke/indent-blankline.nvim",
   dependencies = { "nvim-treesitter" },
-  enabled = true,
   opts = {
-    char = "▏",
-    use_treesitter = true,
-    show_trailing_blankline_indent = false,
-    filetype_exclude = {
-      "help",
-      "alpha",
-      "neo-tree",
-      "NvimTree",
-      "Trouble",
-      "lazy",
-      "norg",
-      "OverseerForm",
+    indent = {
+      char = "▏",
+    },
+    whitespace = { remove_blankline_trail = true },
+    scope = { enabled = true },
+    exclude = {
+      buftypes = {
+        "help",
+        "alpha",
+        "neo-tree",
+        "NvimTree",
+        "Trouble",
+        "lazy",
+        "norg",
+        "OverseerForm",
+      },
     },
   },
+  config = function(_, opts)
+    require("ibl").setup(opts)
+  end,
+  main = "ibl",
   event = "VeryLazy",
 }
 
