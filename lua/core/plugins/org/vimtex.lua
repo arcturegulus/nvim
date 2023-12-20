34486963476bdccc646212cfbca1de7afd6a30f1
Paul Lisondra <arct@arcturegulus.space>
2023-12-20 19:47:23 +0800

    Change vimtex settings for macOS
    Now uses the Skim PDF viewer.

--- lua/core/plugins/org/vimtex.lua
+++ lua/core/plugins/org/vimtex.lua
@@ -3,8 +3,12 @@ local M = {
   "lervag/vimtex",
   lazy = false,
   config = function()
-    vim.g.vimtex_view_general_viewer = "qpdfview"
-    vim.g.vimtex_view_general_options = "--unique @pdf\\#src:@tex:@line:@col"
+    -- vim.g.vimtex_view_general_viewer = "qpdfview"
+    -- vim.g.vimtex_view_general_options = "--unique @pdf\\#src:@tex:@line:@col"
+    
+    vim.g.vimtex_view_method = "skim"
+    vim.g.vimtex_view_skim_sync = 1;
+    vim.g.vimtex_view_skim_no_select = 1;
 
     vim.g.vimtex_compiler_method = "latexmk"
   end,
